0385c6891a6c6023705b2eb077b14617b3e66826
Naoh <27915369+N40h@users.noreply.github.com>
2023-08-10 01:17:11 +0200

    Added a total function to get the sum of all the keys done in a week

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const countElement = document.getElementById('count__element');
 const saveElement = document.getElementById('save__element');
+const totalElement = document.getElementById('total__element');
 const addButton = document.getElementById('btn__add');
 const saveButton = document.getElementById('btn__save');
 const resetButton = document.getElementById('btn__reset');
 
 let count = 0;
+let total = 0;
 
 function increment() {
 	count += 1;
@@ -14,8 +16,14 @@ function increment() {
 function save() {
 	let countStr = count + ' - ';
 	saveElement.textContent += countStr;
+	totalKeys();
 	count = 0;
 	countElement.textContent = count;
+}
+
+function totalKeys() {
+	total += count;
+	totalElement.textContent = 'Total keys done this week: ' + total;
 }
 
 function reset() {
@@ -30,6 +38,7 @@ addButton.addEventListener('click', () => {
 
 saveButton.addEventListener('click', () => {
 	save();
+	totalKeys();
 });
 
 resetButton.addEventListener('click', () => {
